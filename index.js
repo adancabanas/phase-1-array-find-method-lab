6bac75ecd020135bce0be049cdb73a12535c3cf6
Adan Nicolas Cabanas Garza <adan.cabanas@gmail.com>
2023-09-16 23:10:25 -0400

    defines superbowlWin function

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(record) {
+    let yearWon = record.find(r => { return r.result === "W"})
+    return yearWon === undefined ? undefined : yearWon.year;
+}
